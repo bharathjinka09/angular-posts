83f8e7470f09027595c2a22ab0b71238649bde1a
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-27 17:19:17 +0530

    angular posts

--- src/app/model/bloglist.ts
+++ src/app/model/bloglist.ts
@@ -1,6 +1,6 @@
 import { Blog } from './bloghub';
 export let blogs: Blog[] = [
-  new Blog(1, 'Django', 'Python web dev', 'technical'),
-  new Blog(2, 'Interview', 'Amazing Experience', 'personal'),
-  new Blog(3, 'Trip to kerala', 'Awesome!:-)', 'nature'),
+  // new Blog(1, 'Django', 'Python web dev', 'technical'),
+  // new Blog(2, 'Interview', 'Amazing Experience', 'personal'),
+  // new Blog(3, 'Trip to kerala', 'Awesome!:-)', 'nature'),
 ];
